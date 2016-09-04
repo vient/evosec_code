bf708c20844d1d408fae4e217eacaf44bff881f7
Emil Guseynov <theluckyemil@gmail.com>
2016-09-04 18:30:23 +0300

    Added gui

---
+++ code/gui/window.cpp
@@ -0,0 +1,105 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtWidgets>
+
+#include "window.h"
+#include <QDebug>
+
+//! [0]
+Window::Window(QWidget *parent)
+    : QWidget(parent)
+{
+    browseButton = createButton(tr("&Browse..."), SLOT(browse()));
+    directoryComboBox = createComboBox(QDir::currentPath());
+    directoryLabel = new QLabel(tr("In directory:"));
+    scanButton = createButton(tr("&Scan..."), SLOT(scan()));
+    connect(scanButton, &QAbstractButton::clicked, this, &Window::find);
+    //createFilesTable();
+//! [0]
+
+//! [1]
+    QGridLayout *mainLayout = new QGridLayout;
+    mainLayout->addWidget(directoryLabel, 2, 0);
+    mainLayout->addWidget(directoryComboBox, 2, 1);
+    mainLayout->addWidget(browseButton, 2, 2);
+    mainLayout->addWidget(scanButton, 4, 2);
+    setLayout(mainLayout);
+
+    setWindowTitle(tr("Find Files"));
+    resize(700, 300);
+}
+
+void Window::browse()
+{
+    QString directory = QFileDialog::getOpenFileName(this,
+                               tr("Find Files"), directoryComboBox->currentText());
+
+    if (!directory.isEmpty()) {
+        if (directoryComboBox->findText(directory) == -1)
+            directoryComboBox->addItem(directory);
+        directoryComboBox->setCurrentIndex(directoryComboBox->findText(directory));
+    }
+}
+
+QPushButton *Window::createButton(const QString &text, const char *member)
+{
+    QPushButton *button = new QPushButton(text);
+    connect(button, SIGNAL(clicked()), this, member);
+    return button;
+}
+
+QComboBox *Window::createComboBox(const QString &text)
+{
+    QComboBox *comboBox = new QComboBox;
+    comboBox->setEditable(true);
+    comboBox->addItem(text);
+    comboBox->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+    return comboBox;
+}
+
+void Window::scan()
+{
+    QString path = directoryComboBox->currentText();
+    QString filename = QFileInfo(path).fileName();
+    QFile inputFile(filename);
+    qDebug() << inputFile.fileName();
+}
+
